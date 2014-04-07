92b6cff2a88271b6796e97342460cff0f1f52861
NahyanBinKhalid <nahyan.bin.khalid@gmail.com>
2014-04-07 10:07:22 +0500

    Revert "Initial OpenM4S Design"
    This reverts commit 2cae71cc877606619196ca3cbbbcb442dedb372d.

--- m4s/index.php
+++
@@ -1,9 +0,0 @@
-<?php require_once "template/doctype.php"; ?>
-<?php require_once "includes/functions.php"; ?>
-<?php
-	if(isset($_SESSION) && sizeof($_SESSION) > 0) {
-		header('Location: dashboard.php');
-	} else {
-		header('Location: login.php');
-	}
-?>
